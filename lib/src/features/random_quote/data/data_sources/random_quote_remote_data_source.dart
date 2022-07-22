7f560eaef9faecb77695364b5d5a1852e4215a2a
Umar Hasan <umrhsnE2020@gmail.com>
2022-07-22 15:05:55 +0200

    implementation of remote data source

--- lib/src/features/random_quote/data/data_sources/random_quote_remote_data_source.dart
+++ lib/src/features/random_quote/data/data_sources/random_quote_remote_data_source.dart
@@ -1,5 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quotes/src/core/api/endpoint.dart';
+import 'package:quotes/src/core/error/exceptions.dart';
 import 'package:quotes/src/features/random_quote/data/models/quote_model.dart';
 
 abstract class RandomQuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
+}
+
+class RandomQuoteRemoteDataSourceImpl implements RandomQuoteRemoteDataSource {
+  http.Client client;
+
+  RandomQuoteRemoteDataSourceImpl({required this.client});
+
+  /// access api
+  /// TODO: api section will be refined
+  @override
+  Future<QuoteModel> getRandomQuote() async {
+    final randomQuoteUrl = Uri.parse(Endpoint.randomQuote);
+    final response = await client
+        .get(randomQuoteUrl, headers: {'Content-Type': 'application/json'});
+    if (response.statusCode == 200) {
+      return QuoteModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
