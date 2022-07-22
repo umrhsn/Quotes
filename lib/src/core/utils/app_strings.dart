ccd2be65aaac059a6fca6936624b494888779da5
Umar Hasan <umrhsnE2020@gmail.com>
2022-07-22 13:57:09 +0200

    implementation of local data source

--- lib/src/core/utils/app_strings.dart
+++ lib/src/core/utils/app_strings.dart
@@ -2,4 +2,5 @@ class AppStrings {
   static const String appName = 'Quotes';
   static const String fontFamily = 'Product Sans';
   static const String noRouteFound = 'No Route Found';
+  static const String cachedRandomQuote = 'CACHED_RANDOM_QUOTE';
 }
--- lib/src/features/random_quote/data/data_sources/random_quote_local_data_source.dart
+++ lib/src/features/random_quote/data/data_sources/random_quote_local_data_source.dart
@@ -1,6 +1,39 @@
+import 'dart:convert';
+
+import 'package:quotes/src/core/error/exceptions.dart';
+import 'package:quotes/src/core/utils/app_strings.dart';
 import 'package:quotes/src/features/random_quote/data/models/quote_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class RandomQuoteLocalDataSource {
   Future<QuoteModel> getLastRandomQuote();
-  Future<QuoteModel> cacheQuote(QuoteModel quoteModel);
+
+  Future<void> cacheQuote(QuoteModel quote);
+}
+
+/// implementation of local data source
+/// depending on shared_preferences package
+class RandomQuoteLocalDataSourceImpl implements RandomQuoteLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  RandomQuoteLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<QuoteModel> getLastRandomQuote() {
+    final jsonString =
+        sharedPreferences.getString(AppStrings.cachedRandomQuote);
+    if (jsonString != null) {
+      final cacheRandomQuote =
+          Future.value(QuoteModel.fromJson(json.decode(jsonString)));
+      return cacheRandomQuote;
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheQuote(QuoteModel quote) {
+    return sharedPreferences.setString(
+        AppStrings.cachedRandomQuote, json.encode(quote));
+  }
 }
